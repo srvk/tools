f57ca7c92ded711453a50eb2a6517c9f6a652c0e
julien <julien.karadayi@gmail.com>
2018-03-16 14:57:36 +0000

    add eval script

---
+++ eval.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Since the script is built to be launched outside of the vm, source
+# the .bashrc which is not necessarily sourced!
+source ~/.bashrc
+
+# Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+# Absolute path this script is in. /home/user/bin
+BASEDIR=`dirname $SCRIPT`
+
+if [ $# -ne 2 ]; then
+  echo "Usage: eval.sh <data> <system>"
+  echo "where data is the folder containing the data"
+  echo "and system is the system you want"
+  echo "to evaluate. Choices are:"
+  echo "  -ldc_sad"
+  echo "  -noisemes_sad"
+  echo "  -diartk"
+  exit
+fi
+
+# switch eval depending on system
+audio_dir=$1
+system=$2
+
+case $system in
+"ldc_sad"|"noisemes_sad")
+   sh evalSAD.sh $audio_dir $system
+   ;;
+"diartk")
+   sh evalDiar.sh $audio_dir
+   ;;
+esac
