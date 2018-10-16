2559a2ea49b37163bb52ad24876243ef899bf1f7
Eric Riebling <riebling@gmail.com>
2018-10-16 19:41:53 +0000

    work with multiple SAD

--- yuniSeg.sh
+++ yuniSeg.sh
@@ -13,19 +13,28 @@ SCRIPT=$(readlink -f $0)
 BASEDIR=`dirname $SCRIPT`
 # Path to YuniSeg (go one folder up and to Yunitator)
 YUNITATDIR=$(dirname $BASEDIR)/Yunitator
+# let's get our bearings: set CWD to the path of Yunitator
+cd $YUNITATDIR
 
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <dirname>"
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <dirname> <SADtoolname>"
   echo "where dirname is the name of the folder"
   echo "containing the wav and rttm files"
+  echo "and SADtoolname is the SAD to use."
+  echo "Choices are:"
+  echo "  ldc_sad"
+  echo "  noisemes"
+  echo "  tocombosad"
+  echo "  opensmile"
+  echo "  textgrid"
+  echo "  eaf"
+  echo "  rttm"
   exit 1
 fi
 
 audio_dir=/vagrant/$1
-filename=$(basename "$audio_dir")
-dirname=$(dirname "$audio_dir")
-extension="${filename##*.}"
-basename="${filename%.*}"
+trs_format=$2
+
 # Check audio_dir to see if empty or if contains empty wav
 bash $BASEDIR/check_folder.sh $audio_dir
 
@@ -33,23 +42,72 @@ bash $BASEDIR/check_folder.sh $audio_dir
 # this is set in user's login .bashrc, but may not be if run from outside VM
 export PATH=/home/${user}/anaconda/bin:$PATH
 
-# let's get our bearings: set CWD to the path of Yunitator
-cd $YUNITATDIR
 
 # Iterate over files
 echo "Starting"
 for f in `ls $audio_dir/*.wav`; do
-    ./runYuniSeg.sh $f
+    filename=$(basename "$f")
+    basename="${filename%.*}"
+    echo "treating $basename"
+    
+    # output filename produced by runYuniSegs
+    outfile=$audio_dir/$basename.yuniSeg.rttm
+
+    case $trs_format in
+      "ldc_sad")
+       sys="ldcSad"
+      ;;
+      "")
+       # add default case
+       echo "Warning: no SAD source specified, using Noisemes by default, at your own risk."
+       echo "Next time, please specify SAD."
+       sys="noisemesSad"
+      ;;
+      "noisemes")
+       sys="noisemesSad"
+      ;;
+      "tocombosad")
+       sys="tocomboSad"
+      ;;
+      "opensmile")
+       sys="opensmileSad"
+      ;;
+      "textgrid") 
+       sys="goldSad"
+       $conda_dir/python /home/vagrant/varia/textgrid2rttm.py $audio_dir/${basename}.TextGrid $trs_format_${basename}.rttm
+      ;;
+      "eaf")
+       sys="goldSad"
+       $conda_dir/python /home/vagrant/varia/elan2rttm.py $audio_dir/${basename}.eaf $trs_format_${basename}.rttm
+      ;;
+      "rttm")
+       sys="goldSad"
+       cp $audio_dir/${basename}.rttm $trs_format_${basename}.rttm
+      ;;
+      *)
+       echo "ERROR: please choose SAD system between:"
+       echo "  ldc_sad"
+       echo "  noisemes"
+       echo "  tocombosad"
+       echo "  opensmile"
+       echo "  textgrid"
+       echo "  eaf"
+       echo "  rttm"
+       echo "Now exiting..."
+       exit 1
+      ;;
+    esac
+
+    ./runYuniSegs.sh $f $audio_dir/$trs_format_${basename}.rttm
+    cp $outfile $audio_dir/yuniseg_${sys}_${basename}.rttm
+
+    if [ ! -s $audio_dir/yuniseg_${sys}_${basename}.rttm ]; then
+        # if diarization failed, still write an empty file...
+        touch $audio_dir/yuniseg_${sys}_${basename}.rttm
+    fi
 done
 
 echo "$0 finished running"
-
-# take all the .rttm in $audio_dir/Yunitemp/ and move them to /vagrant/data
-for sad in `ls $audio_dir/Yunitemp/*.yuniSeg.rttm`; do
-    _rttm=$(basename $sad)
-    rttm=$audio_dir/yunitator_${_rttm}
-    mv $sad $rttm
-done
 
 # simply remove hyp and feature
 rm -rf $audio_dir/Yunitemp
